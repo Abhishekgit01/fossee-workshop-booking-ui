cb60048382016b0b6fbbcf4baafc46dd4c778c91
Abhishekgit01 <abhishekgit01@users.noreply.github.com>
2026-04-13 22:21:16 +0530

    Fix: migrate url() to re_path() for Django 4.x compatibility

--- workshop_app/urls_password_reset.py
+++ workshop_app/urls_password_reset.py
@@ -1,20 +1,20 @@
-from django.conf.urls import url
-from django.contrib.auth.views import password_reset, password_reset_confirm,\
-        password_reset_done, password_reset_complete, password_change,\
-        password_change_done
+from django.urls import re_path
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView,\
+        PasswordResetDoneView, PasswordResetCompleteView, PasswordChangeView,\
+        PasswordChangeDoneView
 
 urlpatterns = [
-    url(r'^forgotpassword/$', password_reset,
+    re_path(r'^forgotpassword/$', PasswordResetView.as_view(),
         name="password_reset"),
-    url(r'^password_reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        password_reset_confirm,
+    re_path(r'^password_reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        PasswordResetConfirmView.as_view(),
         name='password_reset_confirm'),
-    url(r'^password_reset/mail_sent/$', password_reset_done,
+    re_path(r'^password_reset/mail_sent/$', PasswordResetDoneView.as_view(),
         name='password_reset_done'),
-    url(r'^password_reset/complete/$', password_reset_complete,
+    re_path(r'^password_reset/complete/$', PasswordResetCompleteView.as_view(),
         name='password_reset_complete'),
-    url(r'^changepassword/$', password_change,
+    re_path(r'^changepassword/$', PasswordChangeView.as_view(),
         name='password_change'),
-    url(r'^password_change/done/$', password_change_done,
+    re_path(r'^password_change/done/$', PasswordChangeDoneView.as_view(),
         name='password_change_done'),
 ]
